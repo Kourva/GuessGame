17cb90e4f0ee6ffdfb35936d78fda4bd404d7950
Izolabela <118578799+Izolabela@users.noreply.github.com>
2022-11-29 20:10:05 +0330

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -2,7 +2,7 @@
 
 # Simple Guess game with PyAutoGUI
 # With Score
-# GitHub: https://github.com/MemeSec/GuessGame
+# GitHub: https://github.com/Izolabela/GuessGame
 
 import pyautogui
 import random
